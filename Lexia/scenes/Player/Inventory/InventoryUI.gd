ad835b476d4208a9aa80ffd67b35d573a9e510d0
Juan Jonathan <juan.jonathan@ui.ac.id>
2023-12-07 10:06:44 +0700

    Inventory UI

--- Lexia/scenes/Player/Inventory/InventoryUI.gd
+++ Lexia/scenes/Player/Inventory/InventoryUI.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var inv: Inventory = preload("res://scenes/Player/Inventory/PlayerInventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var isOpen = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	updateSlots()
 	close()
+
+func updateSlots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
 
 func _physics_process(delta):
 	if (Input.is_action_just_pressed("Inventory")):
